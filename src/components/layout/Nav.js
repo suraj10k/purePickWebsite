b90e1f617abf25c577155f46db5586bcb4aee1c0
suraj <suraj@pop-os.localdomain>
2021-06-20 18:55:48 +0300

    Added data entery form

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -7,24 +7,22 @@ const Nav= () => {
     const {user,logout} = useAuth()
 
     return (
-        <GridItem colStart={1} colSpan={3} p={3}>
+        
+        <GridItem colStart={1} colSpan={3} p={3} bgGradient="linear(to-l, #20bf55,#01baef)" borderRadius="md">
             <Flex>
                 {user && (
                     <>
                     <Link to ='/'>
-                        <Text fontSize="md" mr={8}>Dashboard</Text>
+                        <Text fontSize="md"   fontWeight="extrabold" mr={8}>Dashboard</Text>
                     </Link>
-                    <Link to='/showData'>
-                    <Text fontSize="md" mr={8}>Data</Text>
-                    </Link>
-                    <Box as="button" onClick={logout}>
-                    <Text fontSize="md" mr={8}>Logout</Text>
+                    <Box as="button"  onClick={logout}>
+                    <Text fontSize="md" fontWeight="extrabold" mr={8}>Logout</Text>
                     </Box>
                     </>
                 )}
                 {!user && (
                   <Link to ='/login'>
-                  <Text fontSize="md" mr={8}>Login</Text>
+                  <Text fontSize="md" fontWeight="extrabold" mr={8}>Login</Text>
               </Link>  
                 )}
             </Flex>
